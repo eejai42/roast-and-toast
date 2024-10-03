92a625f8c2f3234e7116afc339ce9437c4196c34
eejai42 <start@anabstractlevel.com>
2024-10-03 14:12:54 -0500

    First "empty" commit.

---
+++ airtable/airtable-dotnet-api/CLIClassLibrary/SassyMQ/StandardPayload.designer.cs
@@ -0,0 +1,32 @@
+
+using AirtableDirect.CLI.Lib.DataClasses;
+using Newtonsoft.Json;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YP.SassyMQ.Lib.RabbitMQ
+{
+    public partial class StandardPayload
+    {
+        
+        public GameDetail GameDetail { get; set; }
+        public List<GameDetail> GameDetails { get; set; }
+        
+        public Character Character { get; set; }
+        public List<Character> Characters { get; set; }
+        
+        public AppUser AppUser { get; set; }
+        public List<AppUser> AppUsers { get; set; }
+        
+        public Game Game { get; set; }
+        public List<Game> Games { get; set; }
+        
+
+    }
+}
